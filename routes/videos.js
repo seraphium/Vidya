741bb971d9df8fba5c8f938cfa4d53aa83355a6d
seraphium <viperking@163.com>
2016-10-27 15:36:44 +0800

    first commit on MEAN test project

---
+++ routes/videos.js
@@ -0,0 +1,18 @@
+/**
+ * Created by zezhang on 2016/10/27.
+ */
+var express = require('express');
+var router = express.Router();
+
+var monk = require('monk');
+var db = monk('localhost:27017/vidzy');
+
+router.get('/', function(req, res) {
+    var collection = db.get('videos');
+    collection.find({}, function(err, videos){
+        if (err) throw err;
+        res.json(videos);
+    });
+});
+
+module.exports = router;
